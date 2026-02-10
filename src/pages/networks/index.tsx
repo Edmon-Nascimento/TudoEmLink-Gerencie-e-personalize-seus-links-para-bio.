2cadc4268b737dc7a4e316517125dc70a3618c99
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-02-10 11:15:00 -0300

    feat: create routes

---
+++ src/pages/networks/index.tsx
@@ -0,0 +1,7 @@
+export default function Networks(){
+    return(
+        <div>
+            Página Networks
+        </div>
+    )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Admin from "./pages/admin";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Networks from "./pages/networks";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Home/>
+    },
+    {
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"/admin",
+        element:<Admin/>
+    },
+    {
+        path:"/admin/social",
+        element:<Networks/>
+    }
+])

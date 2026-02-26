132045b65486d23f2b8c3cf8040a4f8f36664097
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-02-26 11:43:49 -0300

    feat: create admin page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,9 @@
+import Header from "../../components/header"
+
 export default function Admin(){
     return(
-        <div>
-            Página Login
+        <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+            <Header/>
         </div>
     )
 }
